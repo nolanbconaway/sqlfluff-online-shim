82dd31d9d094fdc083170131a8ef71ba53c3eb5b
Nolan Conaway <nolanbconaway@gmail.com>
2021-06-01 20:08:08 -0400

    add prod server

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 """Mostly a copy of https://help.pythonanywhere.com/pages/RedirectWebApp/."""
+import os
 from urllib.parse import urlparse, urlunparse
 
 from flask import Flask, redirect, request
+from waitress import serve
 
 TO_DOMAIN = "online.sqlfluff.com"
 
@@ -15,3 +17,8 @@ def redirect_to_new_domain():
     urlparts_list = list(urlparts)
     urlparts_list[1] = TO_DOMAIN
     return redirect(urlunparse(urlparts_list), code=301)
+
+
+if __name__ == "__main__":
+    print("Serving production!")
+    serve(app, host="0.0.0.0", port=int(os.getenv("PORT", "5000")))
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# SQLFLuff-Online Shim
+# SQLFluff-Online Shim
 
 This is a quick flask app which redirects traffic from the old SQLFluff Online app 
 (hosted by Heroku) to the new sqlfluff.com domain. All requests are replaced by swapping 
